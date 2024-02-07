6b64ec89bbebc89ed3d157d3d1b8e66e9014e203
Austin Powers <32768287+Austinpowers7@users.noreply.github.com>
2024-02-07 08:41:08 +0100

    Update server.js

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -26,7 +26,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://cosmo-chat-app.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
